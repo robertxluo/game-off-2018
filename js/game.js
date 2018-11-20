7a149cff78ba4780b721402775eba99bf44166e2
Robert <robertxluo@gmail.com>
2018-11-19 18:19:07 -0800

    Remove tutorial assets from rendering

--- js/game.js
+++ js/game.js
@@ -22,8 +22,8 @@ gameScene.init = function () {
 gameScene.preload = function () {
   // this.load.image('background', '../assets/background.png');
   this.load.image('player', '../assets/player.png');
-  this.load.image('dragon', '../assets/dragon.png');
-  this.load.image('treasure', '../assets/treasure.png');
+  //this.load.image('dragon', '../assets/dragon.png');
+  //this.load.image('treasure', '../assets/treasure.png');
 };
 
 // Executed once, after assets were loaded
@@ -42,28 +42,28 @@ gameScene.create = function () {
   this.player.setScale(0.5);
 
   // Goal 
-  this.treasure = this.add.sprite(this.sys.game.config.width - 80, this.sys.game.config.height / 2, 'treasure');
-  this.treasure.setScale(0.6);
+  // this.treasure = this.add.sprite(this.sys.game.config.width - 80, this.sys.game.config.height / 2, 'treasure');
+  // this.treasure.setScale(0.6);
 
   // Group of enemies
-  this.enemies = this.add.group({
-    key: 'dragon',
-    repeat: 5,
-    setXY: {
-      x: 110,
-      y: 100,
-      stepX: 80,
-      stepY: 20
-    }
-  });
+  // this.enemies = this.add.group({
+  //   key: 'dragon',
+  //   repeat: 5,
+  //   setXY: {
+  //     x: 110,
+  //     y: 100,
+  //     stepX: 80,
+  //     stepY: 20
+  //   }
+  // });
 
-  // Scale down enemies
-  Phaser.Actions.ScaleXY(this.enemies.getChildren(), -0.5, -0.5);
+  // // Scale down enemies
+  // Phaser.Actions.ScaleXY(this.enemies.getChildren(), -0.5, -0.5);
 
-  // Set speeds
-  Phaser.Actions.Call(this.enemies.getChildren(), function (enemy) {
-    enemy.speed = Math.random() * 2 + 1;
-  }, this);
+  // // Set speeds
+  // Phaser.Actions.Call(this.enemies.getChildren(), function (enemy) {
+  //   enemy.speed = Math.random() * 2 + 1;
+  // }, this);
 
   this.isPlayerAlive = true;
 }
@@ -91,29 +91,29 @@ gameScene.update = function () {
   }
 
   // Treasure collision
-  if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), this.treasure.getBounds())) {
-    this.gameOver();
-  }
+  // if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), this.treasure.getBounds())) {
+  //   this.gameOver();
+  // }
 
   // Enemy movement
-  let enemies = this.enemies.getChildren();
-  let numEnemies = enemies.length;
+  // let enemies = this.enemies.getChildren();
+  // let numEnemies = enemies.length;
 
-  for (let i = 0; i < numEnemies; i++) {
-    enemies[i].y += enemies[i].speed;
+  // for (let i = 0; i < numEnemies; i++) {
+  //   enemies[i].y += enemies[i].speed;
 
-    if (enemies[i].y >= this.enemyMaxY && enemies[i].speed > 0) {
-      enemies[i].speed *= -1;
-    } else if (enemies[i].y <= this.enemyMinY && enemies[i].speed < 0) {
-      enemies[i].speed *= -1;
-    }
+  //   if (enemies[i].y >= this.enemyMaxY && enemies[i].speed > 0) {
+  //     enemies[i].speed *= -1;
+  //   } else if (enemies[i].y <= this.enemyMinY && enemies[i].speed < 0) {
+  //     enemies[i].speed *= -1;
+  //   }
 
-    // Enemy Collision
-    // if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), enemies[i].getBounds())) {
-    //   this.gameOver();
-    //   break;
-    // }
-  }
+  //   // Enemy Collision
+  //   // if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), enemies[i].getBounds())) {
+  //   //   this.gameOver();
+  //   //   break;
+  //   // }
+  // }
 }
 
 // End the game
